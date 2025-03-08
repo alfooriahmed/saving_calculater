e86faa275407f124b75bdb011ab083d341258483
Ahmed.... <alfooriahmed3@gmail.com>
2025-03-08 17:35:56 +0400

    i have added a new function

--- Ahmed.py
+++ Ahmed.py
@@ -4,3 +4,15 @@ print("hello world")
 numb = [0, 1, 2, 3, 4, 5, 6]
 for x in numb:
   print(x)
+  
+
+
+def add_car(money, saving, car):
+    if money > saving:
+        print("not enough money")
+    else:
+        saving = saving - money
+        car = car + money
+    print("added + money")
+    
+add_car(50, 100, 0)
